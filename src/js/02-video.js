d03ab432f62c4dfd180b41665893079f30bfa96a
Oleksandr Mysan <werasik228@gmail.com>
2023-05-21 15:33:12 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const video = document.querySelector('#vimeo-player');
+const player = new Player(video);
+
+player.ready().then(() => {
+  player.on('timeupdate', throttle(function(data) {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000));
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  const currentTime = localStorage.getItem('videoplayer-current-time');
+  if (currentTime) {
+    player.setCurrentTime(currentTime).catch(error => {
+      console.error('Error setting current time:', error.message);
+    });
+  }
+});
